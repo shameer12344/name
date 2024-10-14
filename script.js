4f7dda059ba3e50b4a43b774c83657982aefdefc
shamer12 <skshahmeershah786@gmail.com>
2024-10-14 11:34:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let myAge = 13;
+alert("I am "+ myAge + " Years old");
+let countVisited = 1;
+alert("You Visted This Website "+countVisited+" Time")
+let birthYear = "My Birth Year is " + 2011;
+alert(birthYear)
+document.getElementById("div1").innerText = birthYear;
+let cartItem = "Muhammad Shahmeer ordered " + 5 + " Shoe(s) on adidas Shoe Store";
+alert(cartItem)
+document.getElementById("div2").innerText = cartItem;
